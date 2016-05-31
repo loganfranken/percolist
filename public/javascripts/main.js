babaab3aaf877adcc78dc0bfde77859210faeea4
Logan Franken <loganfranken@gmail.com>
2016-05-30 22:46:54 -0700

    Displayed locations on map

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,25 @@
 var coffeeTableTemplate = $('#coffee-table-template').html();
+var coffeeMapTemplate = $('#coffee-map-template').html();
 
 geoid.getPosition().then(function(position) {
 
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
 
-    var map = document.querySelector('google-map');
-    map.latitude = lat;
-    map.longitude = lon;
-
     fetch('/locations?lat=' + lat + '&lon=' + lon).then(function(response) {
       response.json().then(function(locations) {
 
-        console.log(locations);
-
         var coffeeTable = Mustache.render(coffeeTableTemplate, { locations: locations });
         $('.coffee-list').html(coffeeTable);
+
+        var mapData = {
+          latitude: lat,
+          longitude: lon,
+          locations: locations
+        };
+
+        var coffeeMap = Mustache.render(coffeeMapTemplate, mapData);
+        $('.coffee-map').html(coffeeMap);
 
       });
     });
